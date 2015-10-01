4ca3a1715cc8e62f791371c5689b08eb53234162
hyunseolim <2rrtum@gmail.com>
2015-10-01 07:11:32 +0000

    Rails Scaffold

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+    def index
+        
+        
+        
+    end    
+        
+        
+        
+    def about    
+        
+    end 
+    
+    def slate1
+        
+    end
+    
+    def slate2
+        
+    end
+    def slate3
+        
+    end
+    
+    
+end
